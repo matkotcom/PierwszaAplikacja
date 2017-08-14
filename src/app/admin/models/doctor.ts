d94e4a427d92396867817e01c7aa4fcf3b258303
Kalina Plak <kalina.plak@comarch.pl>
2017-08-14 10:34:07 +0200

    CR

--- src/app/admin/models/doctor.ts
+++ src/app/admin/models/doctor.ts
@@ -1,3 +1,7 @@
+/*
+CR: Jeśli nie planujesz dodać żadnych metod do klasy Doctor,
+    to chyba lepiej będzie zrobić analogicznie jak w przypadku pacjenta interface zamiast klasy
+*/
 export class Doctor {
     id: number;
     name: string;
--- src/app/core/sidebar/sidebar.component.ts
+++ src/app/core/sidebar/sidebar.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-
+// CR: Tutaj pytanie czy skoro komponent my-header znalazł się w shared, to może sidebar też powinien?
+//     do zastanowienia, bo to kwestia dyskusyjna gdzie umieszczać komponenty używane w całej aplikacji
 @Component({
   selector: 'sidebar',
   templateUrl: './sidebar.component.html',
